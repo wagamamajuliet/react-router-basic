1ede00bac30d8112f0a487cde07da1fa2f264844
wagamamajuliet <drdldrdl@naver.com>
2018-04-17 14:56:19 +0900

    리액트 라우터 베이직"

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Route, Link} from 'react-router-dom';
+
+const App = () => (
+  <div>
+    <Header/>
+  </div>
+);
+
+
+const Header = () => (
+  <header>
+    <h1>My Contacts</h1>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/contacts">Contacts</Link></li>
+      </ul>
+    </nav>
+    <Route exact path="/" component={Welcome}/>
+    <Route path="/contacts" component={Contacts}/>
+  </header>
+);
+
+const Welcome = () => <h1>Welcome to the best contacts app!</h1>
+
+const Contacts = ({match}) => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/contacts/Terran">Terran</Link>
+      </li>
+      <li>
+        <Link to="/contacts/Zerg">Zerg</Link>
+      </li>
+      <li>
+        <Link to="/contacts/Protoss">Protoss</Link>
+      </li>
+    </ul>
+
+    <Route 
+     exact 
+     path={`${match.path}`}
+     render={() => <h3>Please select a contact</h3>}
+    />
+
+    <Route path={`${match.path}/:contactName`} component={Contact} />
+  </div>
+);
+
+const Contact = ({match}) => 
+  `Your friends name is ${match.params.contactName}`;
+
+
+export default App;
